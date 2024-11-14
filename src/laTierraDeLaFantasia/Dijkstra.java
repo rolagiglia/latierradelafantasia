86f5c3e00e0e6d4585c40378ae07f5b9f453208e
rolagiglia <142903251+rolagiglia@users.noreply.github.com>
2024-11-14 10:26:05 -0300

    Add files via upload

--- src/laTierraDeLaFantasia/Dijkstra.java
+++ src/laTierraDeLaFantasia/Dijkstra.java
@@ -1,54 +1,45 @@
 package laTierraDeLaFantasia;
 
-public class Dijkstra {  //O(n^2)
-	int[] costosMin;
-	int[] Predecesores;
-	
-	public void algoritmoDijkstra(int[][] matrizAdy, int nodoInicial,int[]costosMin,int[]pred) {
-				   			 
-		boolean [] vectorS = new boolean[matrizAdy.length];         //vector nodos visitados
-		int nodoMenorCosto=nodoInicial;
-		int menorCosto;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Dijkstra { // O(n^2)
+
+	public void algoritmoDijkstra(int[][] matrizAdy, int nodoInicial, int[] costosMin, int[] pred) {
+		// Implementacion con cola de prioridad
+		PriorityQueue<int[]> colaPrioridad = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+		int n = matrizAdy.length, nuevoCosto,pesoArista;
+		boolean[] visitados = new boolean[n];
+		int[] actual = new int[2]; // actual[0]->nodo  actual[1]->costo)
 				
-		for(int i=0;i<vectorS.length;i++)       
-			vectorS[i]=false;	
-		
-		for(int i=0;i<costosMin.length;i++)   
-			costosMin[i]=matrizAdy[nodoInicial][i];
-		
-		for(int i=0;i<pred.length;i++)      
-			pred[i]=nodoInicial;
-				
-		vectorS[nodoInicial]=true;  
-	
-		for(int n=0;n<matrizAdy.length;n++)   //
-		{			
-			menorCosto=Integer.MAX_VALUE;
-			for(int i=0;i<costosMin.length;i++) {
-				if(costosMin[i]<menorCosto && vectorS[i]==false) {
-					menorCosto=costosMin[i];
-					nodoMenorCosto=i;	
-				}	
-			
-			}			
-			
-			vectorS[nodoMenorCosto]=true;  //agregamos el nodo como visitado						
-			for(int i=0;i<costosMin.length;i++)   
-		    //para cada nodo que no fue visitado buscamos si hay un mejor camino pasando por el nodoMenorCosto
-			{
-				if(vectorS[i]==false && matrizAdy[nodoMenorCosto][i] != Integer.MAX_VALUE)
-					if(costosMin[i]>(costosMin[nodoMenorCosto]+ matrizAdy[nodoMenorCosto][i])) {
-					
-					costosMin[i] = costosMin[nodoMenorCosto]+ matrizAdy[nodoMenorCosto][i];
-					
-					pred[i]=nodoMenorCosto;  //actualizamos el costo y el predecesor si encontramos un mejor camino
-				}					
-			}				
-		}		
-		
-	} 
+		for (int i = 0; i < costosMin.length; i++) {
+			costosMin[i] = Integer.MAX_VALUE;
+			pred[i] = -1;
+		}
+
+		costosMin[nodoInicial] = 0;
+
+		colaPrioridad.add(new int[] { nodoInicial, 0 });
+
+		while (!colaPrioridad.isEmpty()) {
+			actual = colaPrioridad.poll();
+
+			if (visitados[actual[0]])//evito procesar nuevamente un nodo que ya fue visitado
+				continue;            
+			visitados[actual[0]] = true;
+
+			// Actualizar vecinos del nodo actual
+			for (int vecino = 0; vecino < n; vecino++) {
+				pesoArista = matrizAdy[actual[0]][vecino];
+				if (pesoArista != Integer.MAX_VALUE && !visitados[vecino]) {// el nodo no es inalcanzableno y no fue visitado
+					nuevoCosto = actual[1] + pesoArista;
+					if (nuevoCosto < costosMin[vecino]) {
+						costosMin[vecino] = nuevoCosto;
+						pred[vecino] = actual[0];
+						colaPrioridad.add(new int[] { vecino, nuevoCosto });
+					}
+				}
+			}
+		}
+	}
 }
-	
-	
-
-
